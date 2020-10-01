0861b6ecf9526a99519159561e9effe570dbccd8
Alexander Kim <akim3235@gmail.com>
2020-09-30 17:27:30 -0700

    Initial format and style Prototyper page, added new images, and global styles with styles.scss file.
    Co-authored-by: Ronnie <ronniemail20@gmail.com>
    Co-authored-by: Binish <binish21@gmail.com>
    Co-authored-by: Stefan <Stefan.pougatchev@gmail.com>
    Co-authored-by: Liam <liameno16@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   mode: process.env.NODE_ENV,
-  entry: './client/index.js',
+  entry: "./client/index.js",
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
     // allows you to specify the base path for all the assets within our application
-    publicPath: '/'
+    publicPath: "/",
   },
   devServer: {
-    publicPath: '/build/',
+    publicPath: "/build/",
+    // watchContentBase: true,
     // contentBase: path.resolve(__dirname, './client'),
     // redirect 404s to /index.html.
     // historyApiFallback: true,
+    hot: true,
     proxy: {
-      '/': 'http://localhost:3000/',
+      "/": "http://localhost:3000/",
     },
   },
   module: {
@@ -24,28 +26,24 @@ module.exports = {
         test: /\.jsx?/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
       },
       {
         test: /\.s[ac]ss$/i,
         exclude: /node_modules/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-          ]
-      }, 
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
       {
         test: /.(png|jpg|woff|woff2|eot|ttf|svg|gif)$/,
-        loader: 'url-loader?limit=1024000',
+        loader: "url-loader?limit=1024000",
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
-  }
-}
+    extensions: [".js", ".jsx"],
+  },
+};
