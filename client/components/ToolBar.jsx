01c82304841fd9f7c6de12de5bdba74a850b9af8
“StefanPougatchev” <Stefan.Pougatchev@gmail.com>
2020-10-06 20:58:05 -0700

    added a codeInterface component which renders backend response.
    Co-authored-by: Ronnie <ronniemail20@gmail.com>
    Co-authored-by: Binish <binish21@gmail.com>
    Co-authored-by: Alex <akim3235@gmail.com>
    Co-authored-by: Liam <liameno16@gmail.com>

--- client/components/ToolBar.jsx
+++ client/components/ToolBar.jsx
@@ -1,16 +1,30 @@
-import React from "react";
-import { Nav } from "react-bootstrap";
+import React, {useContext} from "react";
+import {ObjectContext} from './ObjectContextProvider';
+import { Nav, Button } from "react-bootstrap";
 import blue from "../../public/images/blue.svg";
 import salmon from "../../public/images/salmon.svg";
 import arrow from "../../public/images/arrow-short.svg";
 import ObjectTypeForm from './ObjectTypeForm'
 
+
 export default function ToolBar() {
+  const [objectListState, setObjectList, nodeObj, setNodeObj, viewCode, setViewCode] = useContext(ObjectContext);
+
   return (
     <div className='toolbar'>
       <Nav className='flex-column'>
         <ObjectTypeForm />
       </Nav>
+        <div className="mx-auto mb-1">
+          <Button 
+          size='sm'
+          variant='primary'
+          type='submit'
+          onClick={() => 
+          viewCode.displayCode ? setViewCode({...viewCode,displayCode:false}) : setViewCode({...viewCode,displayCode:true})}>
+          {viewCode.displayCode ? 'View Graph' : 'View Code'}
+          </Button>
+        </div>
     </div>
   );
 }
