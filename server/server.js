29d8375f6b2d5da3dfe315d1d306be40381a1c12
Binish Ali <binish21@gmail.com>
2020-10-10 15:10:02 -0700

    Add grapQL API and import testSchema
    Co-authored-by: Ronnie <ronniemail20@gmail.com>
    Co-authored-by: Alex <akim3235@gmail.com>
    Co-authored-by: Stefan <Stefan.pougatchev@gmail.com>
    Co-authored-by: Liam <liameno16@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,25 +1,35 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const router = require('./routes');
+const router = require("./routes");
+const { graphqlHTTP } = require("express-graphql");
+const testSchema = require("./schema/testSchema");
 const PORT = 3000;
 
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: testSchema,
+    graphiql: true,
+  })
+);
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use('/build', express.static(path.join(__dirname, '../build')));
+app.use(express.urlencoded({ extended: true }));
+app.use("/build", express.static(path.join(__dirname, "../build")));
 
-app.use('/api', router);
+app.use("/api", router);
 
 // Serve index.html for our routes declared with react router
-app.use('/prototyper', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../client/index.html')));
-app.use('/', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../client/index.html')));
-
-
+app.use("/prototyper", (req, res) =>
+  res.status(200).sendFile(path.resolve(__dirname, "../client/index.html"))
+);
+app.use("/", (req, res) =>
+  res.status(200).sendFile(path.resolve(__dirname, "../client/index.html"))
+);
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}.`);
-  });
-  
+  console.log(`Listening on port ${PORT}.`);
+});
 
-  module.exports = app; 
+module.exports = app;
