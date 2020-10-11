3f2340b67732036b4c0785f33af043f2342ff601
Binish Ali <binish21@gmail.com>
2020-10-10 17:25:42 -0700

    Separate routes into apiRouter and testRouter
    Co-authored-by: Ronnie <ronniemail20@gmail.com>
    Co-authored-by: Alex <akim3235@gmail.com>
    Co-authored-by: Stefan <Stefan.pougatchev@gmail.com>
    Co-authored-by: Liam <liameno16@gmail.com>

--- server/routes/testRouter.js
+++ server/routes/testRouter.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const controller = require("./controller.js");
-const router = express.Router();
+const controller = require("../controller.js");
+const testRouter = express.Router();
 const fs = require("fs");
 const path = require("path");
 
-router.get("/", controller.getInfo, (req, res) => {
+testRouter.get("/", controller.getInfo, (req, res) => {
   return res.status(200).json(res.locals.info);
 });
 
-router.post("/", controller.createNewSchema, (req, res) => {
+testRouter.post("/", controller.createNewSchema, (req, res) => {
   fs.writeFile(
-    path.resolve(__dirname, "./schema/testSchema.js"),
+    path.resolve(__dirname, "../schema/testSchema.js"),
     res.locals.newSchema,
     (err) => {
       if (err) throw err;
@@ -19,4 +19,4 @@ router.post("/", controller.createNewSchema, (req, res) => {
   res.status(200).json(res.locals.newSchema);
 });
 
-module.exports = router;
+module.exports = testRouter;
