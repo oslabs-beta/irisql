2f57d0aadf9d2bcbe0b9d0e64a55829dde3a776e
Ronniegit config --global user.email ronniemail20@gmail.com <ronniemail20@gmail.com>
2020-10-16 20:27:02 -0700

    Update databaseModels.js with MongoDB connection
    Co-authored-by: Binish <binish21@gmail.com>
    Co-authored-by: Alex <akim3235@gmail.com>
    Co-authored-by: Stefan <Stefan.pougatchev@gmail.com>
    Co-authored-by: Liam <liameno16@gmail.com>

--- server/models/databaseModels.js
+++ server/models/databaseModels.js
@@ -1,8 +1,17 @@
 // const { Pool } = require('pg');
+// const mongoose = require('mongoose');
+
+// user input URI
 const myURI = '';
 
 // when use MongoDB
 // const URI = process.env.MONGO_URI || myURI;
+
+// mongoose
+//   .connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
+//   .then((data) => console.log('connected to database'))
+//   .catch((err) => console.log('error: ', err));
+// const db = mongoose.connection;
 
 // when use PostgreSQL
 const URI = process.env.PG_URI || myURI;
