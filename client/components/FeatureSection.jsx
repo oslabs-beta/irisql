29432991f7fcb799dbc11f461c27909ff02dddab
Alexander Kim <akim3235@gmail.com>
2020-10-17 13:29:47 -0700

    Adjust styling, uploaded new demo video Co-authored-by: Ronnie <ronniemail20@gmail.com> Co-authored-by: Binish <binish21@gmail.com> Co-authored-by: Stefan <Stefan.pougatchev@gmail.com> Co-authored-by: Liam <liameno16@gmail.com>

--- client/components/FeatureSection.jsx
+++ client/components/FeatureSection.jsx
@@ -10,55 +10,47 @@ export default function FeatureSection() {
         <h2>Features</h2>
         <hr />
       </div>
-      <CardDeck>
-        <Card>
-          <div className="feature-icons d-flex justify-content-center">
-            <Icon.BoundingBoxCircles color="#29274c" size={50} />
-          </div>
-          <Card.Body>
-            <Card.Title>Build a Schema</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This content is a little bit longer.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
-        <Card>
-          <div className="feature-icons d-flex justify-content-center">
-            <Icon.CodeSquare color="#29274c" size={50} />
-          </div>
-          <Card.Body>
-            <Card.Title>Auto Generated Code</Card.Title>
-            <Card.Text>
-              This card has supporting text below as a natural lead-in to additional
-              content.
-              {' '}
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
-        <Card>
-          <div className="feature-icons d-flex justify-content-center">
-            <Icon.LayersFill color="#29274c" size={50} />
-          </div>
-          <Card.Body>
-            <Card.Title>Connect To A DataBase</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This card has even longer content than the first to
-              show that equal height action.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
-      </CardDeck>
+      <div className="d-flex justify-content-center">
+        <CardDeck>
+          <Card>
+            <div className="feature-icons d-flex justify-content-center">
+              <Icon.BoundingBoxCircles color="#29274c" size={50} />
+            </div>
+            <Card.Body>
+              <Card.Title>Build a Visualized Schema</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <div className="feature-icons d-flex justify-content-center">
+              <Icon.CodeSquare color="#29274c" size={50} />
+            </div>
+            <Card.Body>
+              <Card.Title>Auto Generated Code</Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to additional
+                content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <div className="feature-icons d-flex justify-content-center">
+              <Icon.LayersFill color="#29274c" size={50} />
+            </div>
+            <Card.Body>
+              <Card.Title>Connect To A DataBase</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This card has even longer content than the first to
+                show that equal height action.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardDeck>
+      </div>
     </div>
   );
 }
--- client/components/TeamSection.jsx
+++ client/components/TeamSection.jsx
@@ -14,63 +14,57 @@ export default function TeamSection() {
           <div className="avatar mx-auto">
             <Image style={{ height: '10rem' }} src="https://avatars2.githubusercontent.com/u/22757733?s=460&u=622812862383976ddc3e5c861ffba97f1f8f98e0&v=4" roundedCircle />
           </div>
-          <h5 className="font-weight-bold mt-4 mb-3">Anna Williams</h5>
-          <p className="text-uppercase blue-text"><strong>Graphic designer</strong></p>
+          <h5 className="font-weight-bold mt-4 mb-3">Stefan Pougatchev</h5>
+          <p className="text-uppercase blue-text"><strong>Software Engineer</strong></p>
           <p className="grey-text">
             Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
             adipisci sed quia non numquam modi tempora eius.
-
           </p>
         </div>
         <div className="col-lg-3 col-md-6 mb-lg-0 mb-5">
           <div className="avatar mx-auto">
             <Image style={{ height: '10rem' }} src="https://avatars2.githubusercontent.com/u/24289044?s=460&u=a28ff1c5bc20680ed1f768104f96f2256b554298&v=4" roundedCircle />
           </div>
-          <h5 className="font-weight-bold mt-4 mb-3">Anna Williams</h5>
-          <p className="text-uppercase blue-text"><strong>Graphic designer</strong></p>
+          <h5 className="font-weight-bold mt-4 mb-3">Ronnie Zhan</h5>
+          <p className="text-uppercase blue-text"><strong>Software Engineer</strong></p>
           <p className="grey-text">
             Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
             adipisci sed quia non numquam modi tempora eius.
-
           </p>
         </div>
         <div className="col-lg-3 col-md-6 mb-lg-0 mb-5">
           <div className="avatar mx-auto">
             <Image style={{ height: '10rem' }} src="https://avatars3.githubusercontent.com/u/61030080?s=400&u=2bc5ddab09ea3ee5771d2ee9817361567d8140f7&v=4" roundedCircle />
           </div>
-          <h5 className="font-weight-bold mt-4 mb-3">Anna Williams</h5>
-          <p className="text-uppercase blue-text"><strong>Graphic designer</strong></p>
+          <h5 className="font-weight-bold mt-4 mb-3">Liam McBride</h5>
+          <p className="text-uppercase blue-text"><strong>Software Engineer</strong></p>
           <p className="grey-text">
             Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
             adipisci sed quia non numquam modi tempora eius.
-
           </p>
         </div>
-
       </div>
       <div className="row mt-5 d-flex justify-content-center">
         <div className="col-lg-3 col-md-6 mb-lg-0 mb-5">
           <div className="avatar mx-auto">
             <Image style={{ height: '10rem' }} src="https://ca.slack-edge.com/T01BCGH75GS-U01BR6Z4N0Z-2ca38d00eb49-512" roundedCircle />
           </div>
-          <h5 className="font-weight-bold mt-4 mb-3">Anna Williams</h5>
-          <p className="text-uppercase blue-text"><strong>Graphic designer</strong></p>
+          <h5 className="font-weight-bold mt-4 mb-3">Binish Ali</h5>
+          <p className="text-uppercase blue-text"><strong>Software Engineer</strong></p>
           <p className="grey-text">
             Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
             adipisci sed quia non numquam modi tempora eius.
-
           </p>
         </div>
         <div className="col-lg-3 col-md-6 mb-lg-0 mb-5">
           <div className="avatar mx-auto">
             <Image style={{ height: '10rem' }} src="https://avatars2.githubusercontent.com/u/39292126?s=460&u=f280eb5642b30997564a92157e01a9d04113b8e6&v=4" roundedCircle />
           </div>
-          <h5 className="font-weight-bold mt-4 mb-3">Anna Williams</h5>
-          <p className="text-uppercase blue-text"><strong>Graphic designer</strong></p>
+          <h5 className="font-weight-bold mt-4 mb-3">Alexander Kim</h5>
+          <p className="text-uppercase blue-text"><strong>Software Engineer</strong></p>
           <p className="grey-text">
             Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
             adipisci sed quia non numquam modi tempora eius.
-
           </p>
         </div>
       </div>
